66b89ab35fa6da8a8803078ee4f3c21d63570ba7
alvaroHernandez <alvaro.hernandezi@alumnos.usm.cl>
2020-03-23 20:23:16 -0300

    merge home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Sentence from "./Sentence";
 
 const Home = () => (
     <div className="App">
         <header className="App-header">
-            NO
+            <Sentence />
         </header>
     </div>
 );
