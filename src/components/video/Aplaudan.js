817182f1e3d62be2c6c5f91f5c4a056727939b08
alvaroHernandez <alvaro.hernandezi@alumnos.usm.cl>
2020-03-24 22:05:34 -0300

    change video

--- src/components/video/Aplaudan.js
+++ src/components/video/Aplaudan.js
@@ -5,7 +5,7 @@ function Video() {
         <div className="App">
             <header className="App-header">
                 <video controls>
-                    <source src="pineraaplaudan2.mp4" type="video/mp4" />
+                    <source src="pineraaplaudan3.mp4" type="video/mp4" />
                 </video>
             </header>
         </div>
