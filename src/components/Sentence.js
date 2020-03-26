4726b4ec020117b8fd104c7f5aed46cfc1f52db9
alvaroHernandez <alvaro.hernandezi@alumnos.usm.cl>
2020-03-26 19:40:32 -0300

    blablabla

--- src/components/Sentence.js
+++ src/components/Sentence.js
@@ -3,7 +3,7 @@ import { properties } from '../resources/sentences.js';
 
 class Sentence extends Component{
     render() {
-        const x = Math.floor(Math.random() * 5);
+        const x = Math.floor(Math.random() * properties.length);
         return (
             <div>
                 {properties[x]}
